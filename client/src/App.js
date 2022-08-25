1b052104cd0bcd55b249d84dab4bbacdc5c03255
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-08-25 12:31:31 -0600

    client side updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+// @ts-nocheck
 import React from 'react';
+import Posts from './components/Posts/Posts';
+// import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+// import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png'
 
-const App = () => {
+export const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            <h2>Memories</h2>
+                <img src={memories} alt="memories" height="60" />
+
+            <div>
+                <Posts />
+            </div>
+            <div>
+                <Form />
+            </div>
         </div>
+          
     );
 }
 
-export default App; 
+// export default App; 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post/Post'
+
+function Posts() {
+  return (
+    <>
+        <h1>Posts</h1>
+        <Post />
+        <Post />
+
+    </>
+  )
+}
+
+export default Posts
