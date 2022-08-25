2d6ed097137b04542fb5fd5cd58f422429701941
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-08-25 12:03:03 -0600

    server side updated

--- server/index.js
+++ server/index.js
@@ -3,11 +3,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors'; 
+import dotenv from 'dotenv';
 
+dotenv.config();  
+
+import postRoutes from './routes/posts.js'; 
 const app = express(); 
+const CONNECTION_URL = process.env.DB_CONNECTION; 
+app.use('/posts', postRoutes); 
 
 app.use(bodyParser.json({limit:"30mb", extended: true})); 
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true})); 
 app.use(cors()); 
 
-const CONNECTION_URL = 
+
+
+const PORT = process.env.PORT || 3001; 
+mongoose.connect(CONNECTION_URL).then(()=>{
+    app.listen(PORT,() => console.log('Server is running'))
+}) .catch((error) => console.log(error.message));
---
+++ server/routes/posts.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+import express from 'express';
+
+import { getPosts, createPost } from '../controllers/posts.js'; 
+
+const router = express.Router(); 
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+export default router; 
